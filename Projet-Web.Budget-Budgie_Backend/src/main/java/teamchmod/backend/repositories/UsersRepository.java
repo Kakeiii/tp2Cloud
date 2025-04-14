2f2dd4487150183dfd9599d0273a3401426297f7
kakeiii <luckyta2002@gmail.com>
2025-04-14 13:08:58 -0400

    delete

--- Projet-Web.Budget-Budgie_Backend/src/main/java/teamchmod/backend/repositories/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package teamchmod.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamchmod.backend.model.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-    public Users findByUsername(String username);
-}
